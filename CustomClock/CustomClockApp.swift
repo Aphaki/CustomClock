1b571b40a4971ce3f4aabd6af2f54a27da58b273
Aphaki <kiapha1108@gmail.com>
2022-07-14 16:04:00 +0900

    Initial Commit

---
+++ CustomClock/CustomClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomClockApp.swift
+//  CustomClock
+//
+//  Created by Sy Lee on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct CustomClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
